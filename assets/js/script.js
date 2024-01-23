9cfe1b9261f84327e7e1e2a1690a6ab022f3c4b6
Volodymyr Shtyka <vladimirshtyka@gmail.com>
2024-01-23 20:34:09 +0000

    dynamically generate timeblocks for the workday

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,29 @@ $(document).ready(function () {
     // Display the current day in the header
     $("#currentDay").text(dayjs().format("dddd, MMMM D"));
 
+    // Dynamically generate timeblocks for the workday
+    function generateTimeblocks() {
+        var timeblocksList = $(".timeblocks-list");
+        var timeStart = 9;
+        var timeEnd = 17;
+
+        for (var hour = timeStart; hour <= timeEnd; hour++) {
+            // Create a new time block element
+            var timeblock = $("<div>").addClass("row align-content-center time-block past");
+
+            // Create elements for hour, textarea, and save button
+            var hourDiv = $("<div>").addClass("col hour").text(hour);
+            var textArea = $("<textarea>").addClass("col description").attr("id", `description-${hour}`);
+            var saveBtn = $("<button>").addClass("col saveBtn").html('<i class="fas fa-save"></i>');
+
+            // Append elements to the time block
+            timeblock.append(hourDiv, textArea, saveBtn);
+
+            // Append the time block to the container
+            timeblocksList.append(timeblock);
+        }
+    }
+
+    // Initialize the page with generated time blocks
+    generateTimeblocks();
 })
